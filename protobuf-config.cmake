0f7dddadee6c3babe48d82635a959674aba4fb57
k000 <zimmerk@live.com>
2018-11-27 20:37:27 +0800

    fix some bugs

--- protobuf-config.cmake
+++ protobuf-config.cmake
@@ -1,8 +1,9 @@
 find_path(PROTOBUF_INCLUDE_DIR NAMES google/protobuf/message.h PATHS ${CONAN_INCLUDE_DIRS_PROTOBUF})
-find_library(PROTOBUF_LITE_LIBRARY NAMES libprotobuf-lite PATHS ${CONAN_LIB_DIRS_PROTOBUF})
-find_library(PROTOBUF_LIBRARY NAMES libprotobuf PATHS ${CONAN_LIB_DIRS_PROTOBUF})
-find_library(PROTOC_LIBRARY NAMES libprotoc PATHS ${CONAN_LIB_DIRS_PROTOBUF})
+find_library(PROTOBUF_LITE_LIBRARY NAMES protobuf-lite PATHS ${CONAN_LIB_DIRS_PROTOBUF})
+find_library(PROTOBUF_LIBRARY NAMES protobuf PATHS ${CONAN_LIB_DIRS_PROTOBUF})
+find_library(PROTOC_LIBRARY NAMES protoc PATHS ${CONAN_LIB_DIRS_PROTOBUF})
 find_program(PROTOC_EXECUTABLE NAMES protoc PATHS ${CONAN_BIN_DIRS_PROTOBUF})
+find_package(ZLIB)
 
 # Create imported target protobuf::libprotobuf-lite
 add_library(protobuf::libprotobuf-lite INTERFACE IMPORTED)
@@ -15,14 +16,16 @@ set_target_properties(protobuf::libprotobuf-lite PROPERTIES
 add_library(protobuf::libprotobuf INTERFACE IMPORTED)
 set_target_properties(protobuf::libprotobuf PROPERTIES
   INTERFACE_INCLUDE_DIRECTORIES ${PROTOBUF_INCLUDE_DIR}
-  INTERFACE_LINK_LIBRARIES ${PROTOBUF_LIBRARY} "ZLIB::ZLIB"
+  INTERFACE_LINK_LIBRARIES ${PROTOBUF_LIBRARY}
+  INTERFACE_LINK_LIBRARIES ZLIB::ZLIB
 )
 
 # Create imported target protobuf::libprotoc
 add_library(protobuf::libprotoc INTERFACE IMPORTED)
 set_target_properties(protobuf::libprotoc PROPERTIES
   INTERFACE_INCLUDE_DIRECTORIES ${PROTOBUF_INCLUDE_DIR}
-  INTERFACE_LINK_LIBRARIES ${PROTOC_LIBRARY} "protobuf::libprotobuf"
+  INTERFACE_LINK_LIBRARIES ${PROTOC_LIBRARY}
+  INTERFACE_LINK_LIBRARIES protobuf::libprotobuf
 )
 
 # Create imported target protobuf::protoc
